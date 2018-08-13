c4b6075f770db92b752e73f1e8ec5ab833dac1df
Daniel Smith <daniel.smith@rexsoftware.com.au>
2018-08-13 10:27:37 +1000

    "Master (S.F.T1)"

--- git_at_cmds/work.sh
+++ git_at_cmds/work.sh
@@ -1,4 +1,17 @@
 usage() {
+    cat <<'EOF'
+
+          __                                                 __
+       __/\ \__          __                                 /\ \
+   __ /\_\ \ ,_\        /'_`\_      __  __  __    ___   _ __\ \ \/'\
+ /'_ `\/\ \ \ \/       /'/'_` \    /\ \/\ \/\ \  / __`\/\`'__\ \ , <
+/\ \L\ \ \ \ \ \_     /\ \ \L\ \   \ \ \_/ \_/ \/\ \L\ \ \ \/ \ \ \\`\
+\ \____ \ \_\ \__\    \ \ `\__,_\   \ \___x___/'\ \____/\ \_\  \ \_\ \_\
+ \/___L\ \/_/\/__/     \ `\_____\    \/__//__/   \/___/  \/_/   \/_/\/_/
+   /\____/              `\/_____/
+   \_/__/
+
+EOF
     echo 'Usage: git @ work'
     exit 1
 }
@@ -16,10 +29,18 @@ cmd_work() {
     fi
 
     if [ ! `git branch --list $branch` ]; then
+        AUTOSTASH=1
+        git stash;
+        git checkout develop;
+        git pull;
         git branch $branch;
     fi;
 
     git checkout $branch
+
+    if [ "$AUTOSTASH" == 1 ]; then
+        git stash pop
+    fi;
     # git @ start
     exit 1;
 }
