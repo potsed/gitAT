3101a7ad40b3c9ffd34ef5c449144b48e7f962d3
Daniel Smith <potsed@gmail.com>
2018-10-21 13:13:32 +1000

    "Master [P: git @ F: autostash T: 1 B: ds/squash]"

--- git_at_cmds/dev.sh
+++ git_at_cmds/dev.sh
@@ -2,7 +2,7 @@ cmd_dev() {
     local current="$(git @ branch -c)"
 
     if [ "$current" != "develop" ]; then
-        git stash push --all -m "switched-to-develop"
+        git stash push --include-untracked -m "switched-to-develop"
         git checkout develop;
         git pull
     fi
--- git_at_cmds/master.sh
+++ git_at_cmds/master.sh
@@ -1,7 +1,7 @@
 cmd_master() {
     local current="$(git @ branch -c)"
     if [ "$current" != "master" ]; then
-        git stash push --all -m "switched-to-master"
+        git stash push --include-untracked -m "switched-to-master"
         git checkout master;
         git pull
     fi
