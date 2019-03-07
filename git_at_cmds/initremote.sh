da3f3c076dc84b5ea7abd9a35224d2cb8f9c5605
Daniel Smith <potsed@gmail.com>
2019-03-07 12:26:53 +1000

    initremote

--- git_at_cmds/initremote.sh
+++ git_at_cmds/initremote.sh
@@ -4,10 +4,16 @@ cmd_initremote() {
         usage; exit 0
     elif [ "$#" -eq 1 ]; then
         git init
-        git remote add origin $1;
-        git add .;
-        git commit -m "Initial commit";
-        git push -u origin master;
+        git remote add origin $1
+        git add .
+        git commit -m "Initial commit"
+        git push --set-upstream origin master
+        git checkout -b develop
+        touch CHANGELOG
+        echo "[$(date)]\r- CHANGELOG CREATED\r- INITIAL COMMIT\r\r" >> CHANGELOG
+        git add .
+        git commit -m "CHANGELOG CREATED"
+        git push --set-upstream origin develop;
     fi
     exit 0;
 }
